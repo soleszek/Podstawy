0bd0ec541a46b46e430ca4b70dd6069caafaa50e
Sylwester Oleszek <sylwester.oleszek@gmail.com>
2018-07-21 22:45:35 +0200

    Nowa wersja

--- src/com/sylwesteroleszek/Blok4/Zad30.java
+++ src/com/sylwesteroleszek/Blok4/Zad30.java
@@ -16,27 +16,33 @@ public class Zad30 {
                     System.out.println("Podaj długość boku: ");
                     int bok = skaner.nextInt();
                     int pole = bok * bok;
-                    System.out.println("Pole kwadratu wynosi: " + pole);
+                    int obwod = 4 * bok;
+                    System.out.println("Pole kwadratu wynosi: " + pole + ", obwód: " + obwod);
                     break;
                 case "2":
                     System.out.println("Podaj długości boków a i b: ");
                     int bok1 = skaner.nextInt();
                     int bok2 = skaner.nextInt();
                     int poleProstokata = bok1 * bok2;
-                    System.out.println("Pole prostokąta wynosi: " + poleProstokata);
+                    int obwodProstokata = (2 * bok1) + (2 * bok2);
+                    System.out.println("Pole prostokąta wynosi: " + poleProstokata + ", obwód: " + obwodProstokata);
                     break;
                 case "3":
                     System.out.println("Podaj wartość promienia: ");
                     float promien = skaner.nextFloat();
                     float poleKola = 3.14f * promien;
-                    System.out.println("Pole koła wynosi: " + poleKola);
+                    float obwodKola = 2 * 3.14f * promien;
+                    System.out.println("Pole koła wynosi: " + poleKola + " obwod: " + obwodKola);
                     break;
                 case "4":
-                    System.out.println("Podaj wartości boku a oraz wysokości: ");
+                    System.out.println("Podaj wartości boków a, b, c oraz wysokości h: ");
                     float bokA = skaner.nextFloat();
-                    float h = skaner.nextFloat();
+                    float bokB = skaner.nextFloat();
+                    float bokC = skaner.nextFloat();
+                    float h = Float.parseFloat(skaner.next());
                     float poleTrojkata = 0.5f * bokA * h;
-                    System.out.println("Pole trójkąta wynosi: " + poleTrojkata);
+                    float obwodTrojkata = bokA + bokB + bokC;
+                    System.out.println("Pole trójkąta wynosi: " + poleTrojkata + ", obwód: " + obwodTrojkata);
                     break;
                 default:
                     System.out.println("Niewłaściwy wybór.");

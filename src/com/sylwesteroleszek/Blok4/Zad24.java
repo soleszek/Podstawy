fe3846bd11b4ce4a6a43b99fa138e06c4c440e6a
Sylwester Oleszek <sylwester.oleszek@gmail.com>
2018-07-21 19:43:47 +0200

    lowest int array value

--- src/com/sylwesteroleszek/Blok4/Zad24.java
+++ src/com/sylwesteroleszek/Blok4/Zad24.java
@@ -1,4 +1,35 @@
 package com.sylwesteroleszek.Blok4;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Zad24 {
+    public static void main(String[] args) {
+        int[] tablicaIntow = {3, 56, 567, 89, 0, 12, 23, 145};
+
+        System.out.println(getMax(tablicaIntow));
+        System.out.println(getMin(tablicaIntow));
+    }
+
+    public static int getMax (int[] tablica) {
+        int wynik = 0;
+        int length = tablica.length;
+
+        Arrays.sort(tablica);
+
+        wynik = tablica[length - 1];
+
+        return wynik;
+    }
+
+    public static int getMin (int[] tablica) {
+        int wynik = 0;
+        int length = tablica.length;
+
+        Arrays.sort(tablica);
+
+        wynik = tablica[0];
+
+        return wynik;
+    }
 }

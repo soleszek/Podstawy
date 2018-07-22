8d3724d88edb90f12a2f853ef20c0b11c77242f5
Sylwester Oleszek <sylwester.oleszek@gmail.com>
2018-07-22 06:23:16 +0200

    Is number a prime number?

--- src/com/sylwesteroleszek/Blok4/Zad31.java
+++ src/com/sylwesteroleszek/Blok4/Zad31.java
@@ -5,24 +5,28 @@ import java.util.Scanner;
 public class Zad31 {
     public static void main(String[] args) {
         Scanner skaner = new Scanner(System.in);
-        boolean sprawdzenie = false;
+
+        boolean czyPierwsza = true;
         while (true) {
             System.out.println("Podaj liczbę, żeby sprawdzić czy jest pierwsza: ");
 
             int liczba = skaner.nextInt();
+            int temp = 0;
 
-            for (int i = 2; i < liczba; i++) {
-                if (((liczba / i) % 1) != 1) {
-                    sprawdzenie = true;
-                } else {
-                    sprawdzenie = false;
+            for (int i = 2; i <= liczba / 2; i++) {
+
+                temp = liczba % i;
+
+                if (temp == 0) {
+                    czyPierwsza = false;
+                    break;
                 }
             }
 
-            if (sprawdzenie) {
-                System.out.println("Liczba nie jest pierwsza.");
-            } else {
+            if (czyPierwsza) {
                 System.out.println("Liczba jest pierwsza.");
+            } else {
+                System.out.println("Liczba nie jest pierwsza.");
             }
         }
     }

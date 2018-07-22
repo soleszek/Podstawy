ee1e67a382492af5a8f82fb41fe5b67d73e565cf
Sylwester Oleszek <sylwester.oleszek@gmail.com>
2018-07-22 19:24:44 +0200

    Zad 18 po raz kolejny

--- src/com/sylwesteroleszek/Blok4/Zad18_2.java
+++ src/com/sylwesteroleszek/Blok4/Zad18_2.java
@@ -1,5 +1,59 @@
 package com.sylwesteroleszek.Blok4;
 
 public class Zad18_2 {
-    psvm
+    public static void main(String[] args) {
+        String litery = "aabbbccbb";
+
+        char[] literyTablica = {'a', 'b', 'c'};
+
+        int temp = 0;
+        int countA = 0;
+        int countB = 0;
+        int countC = 0;
+
+        for (int i = 0; i < litery.length(); i++) {
+            if (litery.charAt(i) == literyTablica[0]) {
+                temp++;
+            } else {
+                temp = 0;
+            }
+
+            if (temp > countA) {
+                countA = temp;
+            }
+        }
+
+        for (int i = 0; i < litery.length(); i++) {
+            if (litery.charAt(i) == literyTablica[1]) {
+                temp++;
+            } else {
+                temp = 0;
+            }
+
+            if (temp > countB) {
+                countB = temp;
+            }
+        }
+
+        for (int i = 0; i < litery.length(); i++) {
+            if (litery.charAt(i) == literyTablica[2]) {
+                temp++;
+            } else {
+                temp = 0;
+            }
+
+            if (temp > countC) {
+                countC = temp;
+            }
+        }
+
+        if (countA > countB && countA > countC) {
+            System.out.println("Najdłuższy ciąg litery A wynosi: " + countA);
+        } else if (countB > countA && countB > countC) {
+            System.out.println("Najdłuższy ciąg litery B wynosi: " + countB);
+        } else {
+            System.out.println("Najdłuższy ciąg litery C wynosi: " + countC);
+        }
+
+    }
 }
